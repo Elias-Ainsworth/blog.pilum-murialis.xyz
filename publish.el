e318476c59e9593da6f9d66e97a387fe199df4f0
Elias Ainsworth <pilum-murialis.toge@proton.me>
2025-05-29 23:41:52 -0700

    fix: Close parentheses in publish.el.

--- publish.el
+++ publish.el
@@ -15,7 +15,6 @@
 (global-font-lock-mode t)
 
 (setq base-dir (expand-file-name "."))
-
 (setq org-publish-project-alist
       `(("site-content"
          :base-directory ,(expand-file-name "pages" base-dir)
@@ -24,12 +23,11 @@
          :publishing-function org-html-publish-to-html
          :with-toc nil
          :html-head-include-default-style nil
-		 :html-head "<meta charset=\"UTF-8\">
-			<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-			<link rel=\"stylesheet\" href=\"../assets/style.css\">
-			<link rel=\"icon\" href=\"../assets/favicon.ico\" type=\"image/x-icon\">"
-         ;; :html-head "<link rel=\"stylesheet\" href=\"../assets/style.css\" />")
-         :html-postamble nil
+         :html-head "<meta charset=\"UTF-8\">
+<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+<link rel=\"stylesheet\" href=\"../assets/style.css\">
+<link rel=\"icon\" href=\"../assets/favicon.ico\" type=\"image/x-icon\">"
+         :html-postamble nil)
 
         ("site-static"
          :base-directory ,(expand-file-name "assets" base-dir)
@@ -39,4 +37,3 @@
          :publishing-function org-publish-attachment)
 
         ("site" :components ("site-content" "site-static"))))
-
